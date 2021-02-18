a1c61d0c405c5a602c7bb271c6a38dc3481f2658
Vonter510 <marcin10h@gmail.com>
2021-02-18 15:54:49 +0100

    Formularz Dodawanie/Odświeżanie

--- WPFap.xaml.cs
+++ WPFap.xaml.cs
@@ -32,6 +32,7 @@ namespace DataBase_Marcin
                            ID = d.ID_pracownika,
                            Imie = d.Imie,
                            Nazwisko = d.Nazwisko,
+                           PESEL = d.Pesel,
                            Data_Zatrudnienia = d.Data_przyjecia
                        };
 
@@ -40,10 +41,55 @@ namespace DataBase_Marcin
                 Console.WriteLine(item.ID);
                 Console.WriteLine(item.Imie);
                 Console.WriteLine(item.Nazwisko);
+                Console.WriteLine(item.PESEL);
                 Console.WriteLine(item.Data_Zatrudnienia);
             }
 
             this.WidokPracownik.ItemsSource = dosc.ToList();
+        }
+
+        private void pDodaj_Click(object sender, RoutedEventArgs e)
+        {
+            WypozyczalniaEntities1 db = new WypozyczalniaEntities1();
+
+            pracownicy pracownicyObiekt = new pracownicy()
+            {
+                Imie = textImie.Text,
+                Nazwisko = textNazwisko.Text,
+                Pesel = textPesel.Text,
+                Data_przyjecia = (DateTime)tData.SelectedDate
+            };
+            db.pracownicy.Add(pracownicyObiekt);
+            db.SaveChanges();
+
+        }
+
+        private void podswiez_Click(object sender, RoutedEventArgs e)
+        {
+            WypozyczalniaEntities1 db = new WypozyczalniaEntities1();
+
+            this.WidokPracownik.ItemsSource = db.pracownicy.ToList();
+        }
+
+        private void WidokPracownik_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(this.WidokPracownik.SelectedItems);
+        }
+
+        private void pModyfikuj_Click(object sender, RoutedEventArgs e)
+        {
+            WypozyczalniaEntities1 db = new WypozyczalniaEntities1();
+
+            var z = from d in db.pracownicy
+                    where d.ID_pracownika == 1
+                    select d;
+
+            foreach (var item in z)
+            {
+                MessageBox.Show(item.Imie);
+            }
+
+            db.SaveChanges();
         }
     }
 }
